0a812ecb1370a6fb3247d9ca274d474e1e805bb8
Laufranco <laurafrsofia@gmail.com>
2024-09-07 23:18:38 -0500

    11111111111111

--- src/letra_o_numero.py
+++ src/letra_o_numero.py
@@ -1,26 +1,22 @@
-def evaluar(peso, estatura, edad):
-    # TODO: Coloca aquí el código del ejercicio 8: Índice de masa corporal
-    imc=peso/(estatura**2)
-    if (imc<22.0):
-        if(edad<45):
-            return "bajo"
-    if (imc>=22.0):
-        if(edad<45):
-            return "medio"
-    if (imc<22.0):
-        if(edad>=45):
-            return "medio"
-    if (imc>=22.0):
-        if(edad>=45):
-            return "alto"
+def evaluar(caracter):
+    # Verificar si es un número
+    if '0' <= caracter <= '9':
+        return "Es número"
+    
+    # Verificar si es una letra mayúscula
+    elif 'A' <= caracter <= 'Z':
+        return "Es letra mayúscula"
+    
+    # Verificar si es una letra minúscula
+    elif 'a' <= caracter <= 'z':
+        return "Es letra minúscula"
+    # Si no es letra ni número
+    else:
+        return "No es letra ni número"
 
 if __name__ == '__main__':
-    print("Peso:", end="")
-    peso = int(input())
-    print("Estatura:", end="")
-    estatura = float(input())
-    print("Edad:", end="")
-    edad = int(input())
+    print("Caracter:", end='')
+    caracter = input()
         
-    respuesta = evaluar(peso, estatura, edad)
+    respuesta = evaluar(caracter)
     print(respuesta)
